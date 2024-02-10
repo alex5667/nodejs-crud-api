fc7498f00c2d957efdd8cf4c0cde288c4c6594c6
alex5667 <alex56671111@gmail.com>
2024-02-10 21:36:39 +0200

    feat: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+import path from "path";
+
+const config = {
+  target: ["node"],
+  mode: "production",
+
+  entry: {
+    index: "./src/app.ts",
+    // dataserver: './src/app/dataserver.ts'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
+        exclude: /node_modules/,
+      },
+    ],
+    parser: {
+      javascript: {
+        importMeta: false,
+      },
+    },
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    chunkFormat: "module",
+    filename: "[name].js",
+    path: path.resolve("dist/"),
+  },
+  experiments: {
+    outputModule: true,
+  },
+};
+
+export default config;
